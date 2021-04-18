bb107119c60548c0ba41b8278380bd03869268c3
Dreamy-Z3r0 <34434717+Dreamy-Z3r0@users.noreply.github.com>
2021-04-18 19:17:18 +0700

    Change PWM method; Add debugging method using Serial; Fix existing errors.
    1. Removed inverting fast PWM mode from Timer 1. Timer 1 now operates in normal mode; Pseudo-PWM signals to control stepper motors are generated by outputting HIGH/LOW on the chosen pins.
    2. Outputting on Serial Monitor is now used in order for user to observe the behaviours of some important variables.
    3. Inputting method of push button is currently not operational. Therefore, commands 'Y' (START) and 'N' (STOP) from Serial Monitor are now temporary solution.
    4. Some variables' names have been changed for more professional appearance.
    5. System's behaviour has been adjusted when the START command is initiated.
    6. A flag guarding microstepping factor has been added.
    7. Some problems regarding calculation functions have been resolved.

--- Automatic_Tester/src/Automatic_Tester.ino
+++ Automatic_Tester/src/Automatic_Tester.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 /******************************************************
  *** Macros and variables related to stepper motors ***
@@ -8,90 +9,114 @@
 
 #define STEPPER_MOTORS PORTB
 
-#define A1_EN   PORTB0
-#define A2_EN   PORTB5
+#define A1_EN PORTB0
+#define A2_EN PORTB5
 
-#define A1_DIR  PORTB3
-#define A2_DIR  PORTB4
+#define A1_DIR PORTB3
+#define A2_DIR PORTB4
 
 #define A1_STEP PORTB1
 #define A2_STEP PORTB2
 
-double STEPPER_PWM_PERIOD = 2;    // milliseconds
-double   STEPPER_PWM_DUTY = 75.0; // %  
+#define MICROSTEPPING_FACTOR 4
 
-bool ROTATION_DIRECTION[] = {true, true};  // true (default) - anti-clockwise rotation; false - c lockwise rotation
+double STEPPER_PWM_PERIOD = 0.3;  // milliseconds
+double STEPPER_PWM_DUTY = 95.0; // %
 
-unsigned int STEPS_TO_TAKE[2];
+bool ROTATION_DIRECTION[] = {true, true}; // true (default) - counter-clockwise rotation; false - clockwise rotation
+
+unsigned int STEPS_TO_TAKE[2] = {0, 0};
 
 unsigned int REMAINING_X_STEPS = 0;
 unsigned int REMAINING_Y_STEPS = 0;
 
-float DISPLACEMENT[2];
-float lastPos[2] = {0, 0};
+float lastPos[2] = {114.0, 114.0};
 
 const float positioning_Array[4][4][2] = {
-    { {0,  0}, {38,  0}, {76,  0}, {114,  0} },
-    { {0, 38}, {38, 38}, {76, 38}, {114, 38} },
-    { {0, 76}, {38, 76}, {76, 76}, {114, 76} },
-    { {0,114}, {38,114}, {76,114}, {114,114} }
+    {{0,   0}, {38,   0}, {76,   0}, {114,   0}},
+    {{0,  38}, {38,  38}, {76,  38}, {114,  38}},
+    {{0,  76}, {38,  76}, {76,  76}, {114,  76}},
+    {{0, 114}, {38, 114}, {76, 114}, {114, 114}}
 };
 
 uint8_t x_index = 0,
         y_index = 0;
 
-
 /************************************************************
  *** Macros and flags used for commands from START button ***
  ************************************************************/
 
-#define startButton 3   // Arduino hardware input pin number for START button
+#define startButton 3 // Arduino hardware input pin number for START button
 
-bool startButtonPressed = false;  // Indicates the states of the START button.
+bool startButtonPressed = false; // Indicates the states of the START button.
 
-bool justStarted = false;   // Indicates the first time START button is pressed in one working cycle.
-bool running = false;       // Flag for starting a working cycle of motors.
+bool justStarted = false;          // Indicates the first time START button is pressed in one working cycle.
+bool MOTOR_OPERATING_FLAG = false; // Flag for starting a working cycle of motors.
 
-unsigned long StartButton_HeldTime;   // Used for managing held time of START button for STOP command.
-bool stopCommand = false;   // Stop command is requested by user after START button has been held for some time (3 seconds).
-
-
+unsigned long StartButton_HeldTime; // Used for managing held time of START button for STOP command.
+bool stopCommand = false;           // Stop command is requested by user after START button has been held for some time (3 seconds).
 
 void setup()
 {
-    // Set pin data direction for stepper-controlling pins
-    DDRB |= ((1 << A2_DIR) | (1 << A2_STEP) | (1 << A2_EN));
-    DDRB |= ((1 << A1_DIR) | (1 << A1_STEP) | (1 << A1_EN));
+  Serial.begin(9600);
 
-    // Disable stepper motors initially to avoid any unwanted operations.
-    STEPPER_MOTORS |= ((1 << A2_EN) | (1 << A1_EN));
+  // Set pin data direction for stepper-controlling pins
+  DDRB |= ((1 << A2_DIR) | (1 << A2_STEP) | (1 << A2_EN));
+  DDRB |= ((1 << A1_DIR) | (1 << A1_STEP) | (1 << A1_EN));
 
-    //Set startButton as input
-    pinMode(startButton, INPUT);
+  // Disable stepper motors initially to avoid any unwanted operations.
+  STEPPER_MOTORS |= ((1 << A2_EN) | (1 << A1_EN));
 
-    // Enable interrrupts for INT1
-    attachInterrupt(digitalPinToInterrupt(startButton), startService, FALLING);
+  //Set startButton as input
+  pinMode(startButton, INPUT);
 
-    // Initialize Timer 1 for controlling stepper motors.
-    Initialization(STEPPER_TIMER, STEPPER_PWM_PERIOD, STEPPER_PWM_PERIOD*(STEPPER_PWM_DUTY/100.0));
+  // Enable interrrupts for INT1
+  attachInterrupt(digitalPinToInterrupt(startButton), startService, FALLING);
+
+  // Initialize Timer 1 for controlling stepper motors.
+  Initialization(STEPPER_TIMER, STEPPER_PWM_PERIOD, STEPPER_PWM_PERIOD * (STEPPER_PWM_DUTY / 100.0));
 }
 
 void loop()
 {
+  if (Serial.available())
+  {
+    byte BUFFER[10];
+    Serial.readBytes(BUFFER, 10);
+
+    if ('Y' == BUFFER[0])
+    {
+      Serial.println("Command: Y");
+      if (!MOTOR_OPERATING_FLAG)
+      {
+        MOTOR_OPERATING_FLAG = true;
+        justStarted = true;
+        x_index = 0;
+        y_index = 0;
+      }
+    }
+    else if ('N' == BUFFER[0])
+    {
+      Serial.println("Command: N");
+      stopCommand = true;
+    }
+    else Serial.println("Invalid command!");
+  }
+
   if (startButtonPressed)
   {
-    if (3000 + StartButton_HeldTime <= 0xFFFFFFFF)    // Normal case of timing with millis() function.
+    if (3000 + StartButton_HeldTime <= 0xFFFFFFFF) // Normal case of timing with millis() function.
     {
       unsigned int currentTime = millis();
-      if (currentTime < StartButton_HeldTime)   // Case of unexpected overflowed timer counter.
+      if (currentTime < StartButton_HeldTime) // Case of unexpected overflowed timer counter.
         STOP_command_initialized();
-      else if (millis() - StartButton_HeldTime >= 3000)   // Normal situation.
+      else if (millis() - StartButton_HeldTime >= 3000) // Normal situation.
         STOP_command_initialized();
     }
-    else    // Prevent the case of overflowed timer counter.
+    else // Prevent the case of overflowed timer counter.
     {
       if (millis() >= (3000 - (0xFFFFFFFF - StartButton_HeldTime)))
-        STOP_command_initialized(); 
+        STOP_command_initialized();
     }
   }
 
@@ -100,29 +125,33 @@ void loop()
     PWM_StartStop(STEPPER_TIMER, false, false);   // Stop both stepper motors immediately.
     get_lastPosition();     // Get last position based on the remaing steps the stepper motors were taking.
 
-    STEPS_TO_TAKE[0] = 0;   // Clear stepper motors' movement data.
-    STEPS_TO_TAKE[1] = 0;
-
     REMAINING_X_STEPS = 0;
     REMAINING_Y_STEPS = 0;
 
     stopCommand = false;    // Clear STOP command for new working cycles.
-    running = false; 
+    MOTOR_OPERATING_FLAG = false;
 
     get_lastPosition();
   }
 
-  if ((running) && (x_index < 4) && (y_index < 4))
+  if ((MOTOR_OPERATING_FLAG) && (x_index < 4) && (y_index < 4))
   {
-    run();
+    TEST_RUN_ROUTINE();
 
     if ((4 == x_index) && (4 == y_index))
     {
-      running = false;
+      MOTOR_OPERATING_FLAG = false;
       x_index = 0;
       y_index = 0;
     }
   }
+
+//  if (MOTOR_OPERATING_FLAG)
+//  {
+//    Serial.print(REMAINING_X_STEPS);
+//    Serial.print(",");
+//    Serial.println(REMAINING_Y_STEPS);
+//  }
 }
 
 void startService()
@@ -138,9 +167,9 @@ void buttonReleased()
 {
   detachInterrupt(digitalPinToInterrupt(startButton));
   startButtonPressed = false;
-  if (!running)
+  if (!MOTOR_OPERATING_FLAG)
   {
-    running = true;
+    MOTOR_OPERATING_FLAG = true;
     justStarted = true;
     x_index = 0;
     y_index = 0;
@@ -156,16 +185,15 @@ void STOP_command_initialized()
   attachInterrupt(digitalPinToInterrupt(startButton), startService, FALLING);
 }
 
-void run()
+void TEST_RUN_ROUTINE()
 {
   if (justStarted)    // START command has just been called.
   {
-    justStarted = false;
     moveTo(positioning_Array[0][0][0], positioning_Array[0][0][1]);   // Move the cursor to the origin.
   }
-
-  if ((0 == REMAINING_X_STEPS) && (0 == REMAINING_Y_STEPS))   // The cursor is stationary and ready for the next position.
+  else if ((0 == REMAINING_X_STEPS) && (0 == REMAINING_Y_STEPS))   // The cursor is stationary and ready for the next position.
   {
+    delay(300);
     x_index += 1;       // Next position on x-axis.
 
     if (4 == x_index)   // Cursor has already been to the last position on the x-axis.
@@ -175,13 +203,30 @@ void run()
     }
 
     // Move the cursor the next position on the board.
-    if ((4 > x_index) || (4 > y_index)) moveTo(positioning_Array[y_index][x_index][0], positioning_Array[y_index][x_index][1]);
+    if ((4 > x_index) || (4 > y_index)) 
+      moveTo(positioning_Array[y_index][x_index][0], positioning_Array[y_index][x_index][1]);
   }
 }
 
 void moveTo(float des_X1, float des_Y1)
 {
-  get_lastPosition();   // Update the cursor's latest position
+  if (!justStarted) get_lastPosition();   // Update the cursor's latest position
+  else
+  {
+    justStarted = false;
+    if ((STEPS_TO_TAKE[0] != 0) || (STEPS_TO_TAKE[1] != 0)) get_lastPosition();
+  }
+
+  Serial.print("Move to (");
+  Serial.print(positioning_Array[y_index][x_index][0]);
+  Serial.print(", ");
+  Serial.print(positioning_Array[y_index][x_index][1]);
+  Serial.print(") from (");
+
+  Serial.print(lastPos[0]);
+  Serial.print(", ");
+  Serial.print(lastPos[1]);
+  Serial.println(")");
 
   // Calculate the steps to take by each axis
   float DISPLACEMENT_X = des_X1 - lastPos[0];
@@ -193,29 +238,41 @@ void moveTo(float des_X1, float des_Y1)
   REMAINING_X_STEPS = STEPS_TO_TAKE[0];
   REMAINING_Y_STEPS = STEPS_TO_TAKE[1];
 
+  Serial.print("   DISPLACEMENT_X = ");
+  Serial.print(DISPLACEMENT_X);
+  Serial.print(" -> ");
+  Serial.print(STEPS_TO_TAKE[0]);
+  Serial.println(" steps");
+
+  Serial.print("   DISPLACEMENT_Y = ");
+  Serial.print(DISPLACEMENT_Y);
+  Serial.print(" -> ");
+  Serial.print(STEPS_TO_TAKE[1]);
+  Serial.println(" steps");
+
   /* Run, Barry. Run! */
 
   // Set rotation direction for motors
   if (ROTATION_DIRECTION[0])
-    STEPPER_MOTORS &= ~(1 << A1_DIR);
-  else
     STEPPER_MOTORS |=  (1 << A1_DIR);
+  else
+    STEPPER_MOTORS &=  ~(1 << A1_DIR);
 
   if (ROTATION_DIRECTION[1])
-    STEPPER_MOTORS &= ~(1 << A2_DIR);
-  else
     STEPPER_MOTORS |=  (1 << A2_DIR);
+  else
+    STEPPER_MOTORS &= ~(1 << A2_DIR);
 
   // Enable or disable motors
   if (0 < STEPS_TO_TAKE[0])
     STEPPER_MOTORS &= ~(1 << A1_EN);
   else
-    STEPPER_MOTORS |=  (1 << A1_EN);
+    STEPPER_MOTORS |= (1 << A1_EN);
 
   if (0 < STEPS_TO_TAKE[1])
     STEPPER_MOTORS &= ~(1 << A2_EN);
   else
-    STEPPER_MOTORS |=  (1 << A2_EN);
+    STEPPER_MOTORS |= (1 << A2_EN);
 
   // Start outputing PWM control signals
   PWM_StartStop(STEPPER_TIMER, REMAINING_X_STEPS > 0, REMAINING_Y_STEPS > 0);
@@ -224,14 +281,11 @@ void moveTo(float des_X1, float des_Y1)
 // Update the cursor's latest position
 void get_lastPosition()
 {
-  lastPos[0] += STEPS_TO_DISPLACEMENT(STEPS_TO_TAKE[0]-REMAINING_X_STEPS, ROTATION_DIRECTION[0]);
-  lastPos[1] += STEPS_TO_DISPLACEMENT(STEPS_TO_TAKE[1]-REMAINING_Y_STEPS, ROTATION_DIRECTION[1]);
+  lastPos[0] += STEPS_TO_DISPLACEMENT(STEPS_TO_TAKE[0] - REMAINING_X_STEPS, ROTATION_DIRECTION[0]);
+  lastPos[1] += STEPS_TO_DISPLACEMENT(STEPS_TO_TAKE[1] - REMAINING_Y_STEPS, ROTATION_DIRECTION[1]);
 
   if ((0 == REMAINING_Y_STEPS) && (0 == REMAINING_X_STEPS))
   {
-    STEPS_TO_TAKE[0] = 0;
-    STEPS_TO_TAKE[1] = 0;
-
     save_lastPosition();
   }
 }
@@ -241,51 +295,52 @@ void save_lastPosition()
   // Routine to save latest position to eeprom
 }
 
-
 /*****************************************************
  *** Interchanging steps and physical displacement ***
  *****************************************************/
 float STEPS_TO_DISPLACEMENT(unsigned int STEPS, bool ROTATION_DIRECTION)
 {
-  return (ROTATION_DIRECTION ? 40.0 : -40.0) * STEPS;
+  return ((float)STEPS / ((ROTATION_DIRECTION ? 5.0 : -5.0) * MICROSTEPPING_FACTOR));
 }
 
 void DISPLACEMENT_TO_STEPS(float DISPLACEMENT_MM, bool *ROTATION_DIRECTION, unsigned int *STEPS)
 {
   *ROTATION_DIRECTION = (DISPLACEMENT_MM >= 0);
 
-  float DISTANCE = DISPLACEMENT >= 0 ? DISPLACEMENT_MM : -DISPLACEMENT_MM;
-  *STEPS = (unsigned int)(DISTANCE / 40.0);
+  float DISTANCE = (DISPLACEMENT_MM >= 0 ? DISPLACEMENT_MM : -DISPLACEMENT_MM);
+  *STEPS = (unsigned int)(DISTANCE * 5.0 * MICROSTEPPING_FACTOR);
+  Serial.print("         DISPLACEMENT_MM = ");
+  Serial.println(DISPLACEMENT_MM);
+  Serial.print("         DISTANCE = ");
+  Serial.println(DISTANCE);
+  Serial.print("         Rotation direction: ");
+  Serial.println(*ROTATION_DIRECTION ? "counter-clockwise" : "clockwise");
+  Serial.print("         STEPS = ");
+  Serial.println(*STEPS);
 }
-
 
 /*******************************************
  *** Output control signal(s) for motors ***
  *******************************************/
-const uint32_t MAX_COUNTER_VALUE_TIM1 = 65535;
-uint32_t COMPARE_MATCH_VALUE_TIM1, BASE_COUNTER_VALUE_TIM1;
+uint16_t BASE_COUNTER_LOW_PERIOD, BASE_COUNTER_HIGH_PERIOD;
+bool CURRENT_OUTPUT_VALUE = LOW;
 
-void Initialization(uint8_t TIMER_ID, double PERIOD_MS, double DUTY_CYCLE_MS)    // Called once at boot, or whenever user wants to change control signal.
+void Initialization(uint8_t TIMER_ID, float PERIOD_MS, float DUTY_CYCLE_MS)    // Called once at boot, or whenever user wants to change control signal.
 {
-  double PERIOD = PERIOD_MS / 1000.0;
-  double   DUTY = DUTY_CYCLE_MS / PERIOD_MS;
-
   if (STEPPER_TIMER == TIMER_ID)  // Timer 1 in use
   {
-    TIMER_COUNTER_COMPARE_VALUE_UPDATE(TIMER_ID, PERIOD, DUTY);
-
-    // Set initial value for counter limit and counter compare match value
-    OCR1A = MAX_COUNTER_VALUE_TIM1;
-    OCR1B = MAX_COUNTER_VALUE_TIM1;
-    ICR1 = COMPARE_MATCH_VALUE_TIM1;
-    TCNT1 = BASE_COUNTER_VALUE_TIM1;
-
-    // Set counter mode: Fast PWM mode 15
-    TCCR1A |= ((1 << WGM11) | (1 << WGM10));
-    TCCR1B |= ((1 << WGM13) | (1 << WGM12));
-    
-    // Enable overflow interrupt
     cli();
+
+    TIMER_COUNTER_COMPARE_VALUE_UPDATE(TIMER_ID, PERIOD_MS, DUTY_CYCLE_MS);
+
+    // Set counter mode: Normal mode
+    TCCR1A &= ~((1 << WGM11) | (1 << WGM10));
+    TCCR1B &= ~((1 << WGM13) | (1 << WGM12));
+
+    // Set initial value for counter register
+    TCNT1 = BASE_COUNTER_LOW_PERIOD;
+
+    // Enable overflow interrupt
     TIMSK1 = 1;   // Only enable overflow interrupt
     sei();
   }
@@ -295,79 +350,93 @@ void PWM_StartStop(uint8_t TIMER_ID, bool CHANNEL_A, bool CHANNEL_B)    // Calle
 {
   if (STEPPER_TIMER == TIMER_ID)  // Timer 1 in use
   {
-    if (CHANNEL_B)  // Enable inverted fast PWM on channel B of Timer 1 (pin OC1B - Arduino D10)
-      TCCR1A |=  ((1 << COM1B1) | (1 << COM1B0));
-    else            // Disable inverted fast PWM on channel B of Timer 1 (pin OC1B - Arduino D10)
-      TCCR1A &= ~((1 << COM1B1) | (1 << COM1B0));
-
-    if (CHANNEL_A)  // Enable inverted fast PWM on channel A of Timer 1 (pin OC1A - Arduino D9)
-      TCCR1A |= ((1 << COM1A1) | (1 << COM1A0));
-    else            // Disable inverted fast PWM on channel B of Timer 1 (pin OC1A - Arduino D9)
-      TCCR1A &= ~((1 << COM1A1) | (1 << COM1A0));  
-
     if ((!CHANNEL_B) && (!CHANNEL_A))   // Stop Timer 1: Stop PWM on both pins OC1A and OC1B
       TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
     else    // Timer 1 running: PWM enabled on either OC1A or OC1B, or both pins
     {
-      // Precaler = 64
-      TCCR1B &=  ~(1 << CS12);
-      TCCR1B |=  ((1 << CS11) | (1 << CS10));
+      // Precaler = 8
+      TCCR1B &=  ~((1 << CS12) | (1 << CS10));
+      TCCR1B |=   (1 << CS11);
     }
   }
 }
 
-void TIMER_COUNTER_COMPARE_VALUE_UPDATE(uint8_t TIMER_ID, double PERIOD, double DUTY)
+void TIMER_COUNTER_COMPARE_VALUE_UPDATE(uint8_t TIMER_ID, float PERIOD, float DUTY)
 {
   if (STEPPER_TIMER == TIMER_ID)  // Timer 1 in use
   {
-    BASE_COUNTER_VALUE_TIM1 = 65536 - (uint32_t)(PERIOD * 250000.0);
-    COMPARE_MATCH_VALUE_TIM1 = (uint32_t)(PERIOD * (1 - DUTY) * 250000.0) - 1 + BASE_COUNTER_VALUE_TIM1;
+    BASE_COUNTER_HIGH_PERIOD = (uint16_t)(65536 - 2000 * DUTY);
+    BASE_COUNTER_LOW_PERIOD  = (uint16_t)(65536 - 2000 * (PERIOD - DUTY));
   }
 }
 
 ISR(TIMER1_OVF_vect)
 {
-  bool CHANNEL_A = true,
-       CHANNEL_B = true;
-  /* Routine to check if any motor stops or runs */
-  if (REMAINING_X_STEPS == 0) STEPPER_MOTORS |= (1 << A1_EN);
-  if (REMAINING_Y_STEPS == 0) STEPPER_MOTORS |= (1 << A2_EN); 
-  
-  if ((REMAINING_X_STEPS == 0) && (REMAINING_Y_STEPS == 0))   // Neither motor on x-axis nor motor y-axis has any more steps to run.
+  CURRENT_OUTPUT_VALUE = !CURRENT_OUTPUT_VALUE;
+
+  if (CURRENT_OUTPUT_VALUE)
   {
-    CHANNEL_A = false;
-    CHANNEL_B = false;   
+    TCNT1 = BASE_COUNTER_HIGH_PERIOD;
+
+    if ((REMAINING_X_STEPS == 0) && (REMAINING_Y_STEPS == 0))   // Neither motor on x-axis nor motor y-axis has any more steps to run.
+    {
+      STEPPER_MOTORS &= ~((1 << A1_STEP) | (1 << A2_STEP));
+    }
+    else if ((REMAINING_X_STEPS == 0) ^ (REMAINING_Y_STEPS == 0))   // Either motor on x-axis or motor y-axis stops.
+    {
+      if (REMAINING_X_STEPS == 0)   // Motor on x-axis stops
+      {
+        STEPPER_MOTORS &= ~(1 << A1_STEP);
+        STEPPER_MOTORS |=  (1 << A2_STEP);
+      }
+      else  // Motor on y-axis stops.
+      {
+        STEPPER_MOTORS &= ~(1 << A2_STEP);
+        STEPPER_MOTORS |=  (1 << A1_STEP);
+      }
+    }
+    else
+    {
+      STEPPER_MOTORS |= ((1 << A1_STEP) | (1 << A2_STEP));
+    }
   }
-  else if ((REMAINING_X_STEPS == 0) ^ (REMAINING_Y_STEPS == 0))   // Either motor on x-axis or motor y-axis stops.
+  else
   {
-    if (REMAINING_X_STEPS == 0)   // Motor on x-axis stops
+    TCNT1 = BASE_COUNTER_LOW_PERIOD;
+
+    STEPPER_MOTORS &= ~((1 << A1_STEP) | (1 << A2_STEP));
+
+    bool CHANNEL_A = true,
+         CHANNEL_B = true;
+    /* Routine to check if any motor stops or runs */
+    if (REMAINING_X_STEPS == 0) STEPPER_MOTORS |= (1 << A1_EN);
+    if (REMAINING_Y_STEPS == 0) STEPPER_MOTORS |= (1 << A2_EN);
+
+    if ((REMAINING_X_STEPS == 0) && (REMAINING_Y_STEPS == 0))   // Neither motor on x-axis nor motor y-axis has any more steps to run.
     {
       CHANNEL_A = false;
+      CHANNEL_B = false;
+    }
+    else if ((REMAINING_X_STEPS == 0) ^ (REMAINING_Y_STEPS == 0))   // Either motor on x-axis or motor y-axis stops.
+    {
+      if (REMAINING_X_STEPS == 0)   // Motor on x-axis stops
+      {
+        CHANNEL_A = false;
+        REMAINING_Y_STEPS -= 1;
+      }
+      else  // Motor on y-axis stops.
+      {
+        CHANNEL_B = false;
+        REMAINING_X_STEPS -= 1;
+      }
+    }
+    else  // Both motors keep running.
+    {
+      REMAINING_X_STEPS -= 1;
       REMAINING_Y_STEPS -= 1;
     }
-    else  // Motor on y-axis stops.
-    {
-      CHANNEL_B = false;
-      REMAINING_X_STEPS -= 1;
-    }
-  }
-  else  // Both motors keep running.
-  {
-    REMAINING_X_STEPS -= 1;
-    REMAINING_Y_STEPS -= 1;
-  }
 
-  /* Routine to run or stop motors */
-  if (CHANNEL_A || CHANNEL_B)   // At least one motor keeps running
-  {
-    // Update timer parameters as required by the mode of timer (described in the datasheet)
-    if (CHANNEL_A) OCR1A = MAX_COUNTER_VALUE_TIM1;
-    if (CHANNEL_B) OCR1B = MAX_COUNTER_VALUE_TIM1;
-
-    ICR1 = COMPARE_MATCH_VALUE_TIM1;
-    TCNT1 = BASE_COUNTER_VALUE_TIM1;
+    // Keep the enabled motor(s) running and stop the disabled motor(s)
+    PWM_StartStop(STEPPER_TIMER, CHANNEL_A, CHANNEL_B);
   }
-
-  // Keep the enabled motor(s) running and stop the disabled motor(s)
-  PWM_StartStop(STEPPER_TIMER, CHANNEL_A, CHANNEL_B);
 }
